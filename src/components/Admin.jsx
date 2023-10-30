105e134ade9b15f0d03a639764657acafdf8048d
mborzechowsk <m.borzechowski@gmail.com>
2023-10-30 01:05:20 +0100

    dodawanie nowych pytań

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,5 +1,5 @@
 import {Link} from 'react-router-dom';
-import {useEffect, useState} from "react";
+import {useEffect, useState, useRef} from "react";
 import supabase from "../services/supabase";
 import closeButton from "../assets/x.png"
 import {useAuth} from "../services/AuthContext.jsx";
@@ -9,9 +9,17 @@ export default function Admin() {
     const [openModalAdd, setOpenModalAdd] = useState(null);
     const {user, login, logout} = useAuth();
     const [items, setItems] = useState([]);
-    const [questions, setQuestions] = useState([])
-    const [rooms, setRooms] = useState([])
-    const [types, setTypes] = useState([])
+    const [questions, setQuestions] = useState([]);
+    const [rooms, setRooms] = useState([]);
+    const [types, setTypes] = useState([]);
+    const [selectedOption, setSelectedOption] = useState('');
+    const [otherRoom, setOtherRoom] = useState('...');
+    const [otherType, setOtherType] = useState('...');
+    const [name, setName] = useState('');
+    const [type, setType] = useState('');
+    const inputRoomRef = useRef(null);
+    const inputTypeRef = useRef(null);
+    const [addQuestion, setAddQuestion] = useState('');
     const deleteIcon = <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 60 60" id="delete"
                             stroke="black" fill="black">
         <path d="M18.3,56h23.6c2.7,0,4.8-2.2,4.8-4.8V18.7h2.1c0.6,0,1-0.4,1-1v-2.3c0-2.1-1.7-3.7-3.7-3.7h-8.5V9.1c0-1.7-1.4-3.1-3.1-3.1
@@ -21,13 +29,18 @@ export default function Admin() {
         <path
             d="M37.9 49.2c.6 0 1-.4 1-1V24.4c0-.6-.4-1-1-1s-1 .4-1 1v23.8C36.9 48.8 37.4 49.2 37.9 49.2zM30.1 49.2c.6 0 1-.4 1-1V24.4c0-.6-.4-1-1-1s-1 .4-1 1v23.8C29.1 48.8 29.5 49.2 30.1 49.2zM22.2 49.2c.6 0 1-.4 1-1V24.4c0-.6-.4-1-1-1s-1 .4-1 1v23.8C21.2 48.8 21.6 49.2 22.2 49.2z"></path>
     </svg>
+    const saveIcon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" id="save" width="35" height="35"
+                          stroke="none" fill="blue">
+        <path
+            d="M15.173 2H4c-1.101 0-2 .9-2 2v12c0 1.1.899 2 2 2h12c1.101 0 2-.9 2-2V5.127L15.173 2zM14 8c0 .549-.45 1-1 1H7c-.55 0-1-.451-1-1V3h8v5zm-1-4h-2v4h2V4z"></path>
+    </svg>
 
     useEffect(() => {
         getItems();
         getQuestions()
         getRooms()
         getType()
-    }, [openModal]);
+    }, [openModal, openModalAdd]);
 
 
     const handleDelete = async (itemId, query) => {
@@ -113,9 +126,78 @@ export default function Admin() {
         setOpenModal(null);
     };
 
+    const handleCloseAddModal = () => {
+        setOpenModalAdd(null);
+    };
     const handleAddButton = (modalId) => {
         setOpenModalAdd(modalId);
     };
+
+    const handleOtherRoom = () => {
+        setSelectedOption(inputRoomRef.current.value);
+        setOtherRoom(inputRoomRef.current.value);
+
+    }
+    const handleOtherType = () => {
+        setType(inputTypeRef.current.value);
+        setOtherType(inputTypeRef.current.value);
+    }
+    const handleSaveItemButton = async () => {
+        try {
+
+            const itemsId = items.map(item => item.id)
+            const highestId = Math.max(...itemsId)
+                       const roomToSave = selectedOption === otherRoom ? inputRoomRef.current.value : selectedOption;
+            const typeToSave = type === otherType ? inputTypeRef.current.value : type;
+
+            const dataToSave = {
+                id: highestId + 1,
+                name: name,
+                type: typeToSave,
+                room: roomToSave,
+            };
+
+            const {data, error} = await supabase
+                .from('Query Items')
+                .insert([dataToSave])
+                .select()
+
+            if (error) {
+                throw error;
+            }
+
+            setOpenModalAdd(null)
+        } catch (error) {
+            console.error('Coś poszło nie tak:', error);
+        }
+    }
+
+    const handleSaveQuestionButton = async () => {
+        try {
+
+            const questionsId = questions.map(item => item.id)
+            const highestId = Math.max(...questionsId)
+
+
+            const dataToSave = {
+                id: highestId + 1,
+                question: addQuestion,
+            };
+
+            const {data, error} = await supabase
+                .from('questions')
+                .insert([dataToSave])
+                .select()
+
+            if (error) {
+                throw error;
+            }
+
+            setOpenModalAdd(null)
+        } catch (error) {
+            console.error('Coś poszło nie tak:', error);
+        }
+    }
 
     return (
         <>
@@ -125,8 +207,10 @@ export default function Admin() {
                     <>
                         <div className="modal_overlay">
                             <div className="modal_content">
-                                <button onClick={handleCloseModal}>X</button>
-
+                                <button onClick={handleCloseModal} className="close_btn"><img src={closeButton}
+                                                                                              alt="X"
+                                                                                              className="close_btn"/>
+                                </button>
                             </div>
                         </div>
                     </>
@@ -150,11 +234,16 @@ export default function Admin() {
                                 {openModalAdd === "add_item" && (
                                     <>
                                         <div className="add_item_modal">
-                                            <button onClick={handleCloseModal} className="close_btn_add_modal"><img
-                                                src={closeButton}
-                                                alt="X"
-                                                className="close_btn"/>
-                                            </button>
+                                            <div className="modal_buttons">
+                                                <button onClick={handleCloseAddModal} className="close_btn_add_modal">
+                                                    <img
+                                                        src={closeButton}
+                                                        alt="X"
+                                                        className="close_btn"/>
+                                                </button>
+                                                <button onClick={handleSaveItemButton}
+                                                        className="save_btn">{saveIcon}</button>
+                                            </div>
                                             <table className="items_table">
                                                 <thead>
                                                 <tr>
@@ -167,25 +256,45 @@ export default function Admin() {
                                                 </thead>
                                                 <tbody>
                                                 <tr>
-                                                    <td><input type="text" placeholder="..."/></td>
-                                                    <td><select name="pokój" id="">
+                                                    <td><input type="text" placeholder="..." value={name}
+                                                               onChange={(e) => setName(e.target.value)}/></td>
+                                                    <td><select name="pokój" id="room" value={selectedOption}
+                                                                onChange={(e) => setSelectedOption(e.target.value)}>
                                                         {
                                                             rooms.map(room => {
                                                                 return (
-                                                                    <option key={room} value={room}>{room}</option>
+                                                                    <option key={room}>{room}</option>
                                                                 )
                                                             })
                                                         }
-                                                        <option value="custom">Inny</option>
+                                                        <option>{otherRoom}</option>
                                                     </select>
-                                                    </td>
-                                                    <td><select name="typ" id="">{
-                                                        types.map(type => {
-                                                            return (
-                                                                <option key={type} value={type}>{type}</option>
+                                                        {
+                                                            selectedOption === otherRoom && (
+                                                                <>
+                                                                    <input ref={inputRoomRef}/>
+                                                                    <button onClick={handleOtherRoom}>OK</button>
+                                                                </>
                                                             )
-                                                        })
-                                                    }</select></td>
+                                                        }
+                                                    </td>
+                                                    <td><select name="typ" id="" value={type}
+                                                                onChange={(e) => setType(e.target.value)}>
+                                                        {
+                                                            types.map(type => {
+                                                                return (
+                                                                    <option key={type}>{type}</option>
+                                                                )
+                                                            })
+                                                        }
+                                                        <option>{otherType}</option>
+                                                    </select>{type === otherType && (
+                                                        <>
+                                                            <input ref={inputTypeRef} />
+                                                            <button onClick={handleOtherType}>OK</button>
+                                                        </>
+                                                    )}
+                                                    </td>
                                                     <td><input type="file"/></td>
                                                 </tr>
                                                 </tbody>
@@ -235,9 +344,38 @@ export default function Admin() {
                     <>
                         <div className="modal_overlay">
                             <div className="modal_content">
-                                <button onClick={handleCloseModal} className="close_btn"><img src={closeButton} alt="X"
-                                                                                              className="close_btn"/>
-                                </button>
+                                <div className="modal_buttons">
+                                    <button onClick={handleCloseModal} className="close_btn"><img src={closeButton}
+                                                                                                  alt="X"
+                                                                                                  className="close_btn"/>
+                                    </button>
+                                    <button onClick={() => handleAddButton("add_question")} className="close_btn"><img
+                                        src={closeButton}
+                                        alt="X"
+                                        className="add_btn"/>
+                                    </button>
+                                </div>
+                                {openModalAdd === "add_question" && (
+                                    <>
+                                        <div className="add_item_modal">
+                                            <div className="modal_buttons">
+                                                <button onClick={handleCloseAddModal} className="close_btn_add_modal">
+                                                    <img
+                                                        src={closeButton}
+                                                        alt="X"
+                                                        className="close_btn"/>
+                                                </button>
+                                                <button onClick={handleSaveQuestionButton}
+                                                        className="save_btn">{saveIcon}</button>
+                                            </div>
+                                            <div className="modal_adding_question">
+                                                <textarea name="newQuestion" id="newQuestion" rows={1} className="adding_question" placeholder="dodaj pytanie" onChange={(e) => setAddQuestion(e.target.value)}></textarea>
+                                            </div>
+                                        </div>
+                                    </>
+                                )
+
+                                }
                                 <table className="question_table">
                                     <thead>
                                     <tr>
