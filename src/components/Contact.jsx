a9013f0ac67a6689c625e0032b342eacdc1b3590
mborzechowsk <m.borzechowski@gmail.com>
2023-11-03 11:38:07 +0100

    poprawienie logowania

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 // import contact_foto from '../assets/contact.png'
-import logo from '../assets/projekt-kontakt.png'
+// import logo from '../assets/projekt-kontakt.png'
 import insta from '../assets/insta.webp'
 import {Link} from "react-router-dom";
 
@@ -8,7 +8,7 @@ export default function Contact(){
 
     return (
         <div className="container container_contact">
-            <img src={logo} alt="logo" className="logo_contact"/>
+            {/*<img src={logo} alt="logo" className="logo_contact"/>*/}
             {/*<img src={contact_foto} alt="architektka"/>*/}
             <div className="contact_name"><p className="contact_text">Agata Tofel</p>
             <p className="contact_text">2.02.1986</p></div>
